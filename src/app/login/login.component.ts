b537777ceb029354ff24cd50726df5a6652e78c7
Kevin Geronimo <96751432+kevingeronimo@users.noreply.github.com>
2022-07-22 18:08:47 +0000

    chore: setup basic authentication/authorization

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { catchError, of, tap } from 'rxjs';
+import { AuthService } from '../auth.service';
+import { TokenStorageService } from '../token-storage.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  isLoggedIn = false;
+  isLoginFailed = false;
+  errorMessage = '';
 
+  constructor(private authService: AuthService, private tokenStorage: TokenStorageService) { }
+  
   ngOnInit(): void {
+    if (this.tokenStorage.getToken()) {
+      this.isLoggedIn = true;
+    }
+  }
+
+  onSubmit(form: NgForm): void {
+    const { username, password } = form.value;
+    console.log(password)
+    this.authService.login(username, password)
+    .pipe(
+      tap(data => {
+        console.log(data)
+        this.tokenStorage.saveToken(data['access_token']);
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+      }),
+      catchError((e =>  {
+        this.errorMessage = e.message;
+        this.isLoginFailed = true;
+        return of(`Error: ${e.message}`)
+      }))
+    ).subscribe()
+
+    form.reset()
   }
 
 }
