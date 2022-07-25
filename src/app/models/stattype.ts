a5e501fc6e0ceb72fe18d3b50c220c36c0a5e489
Adam Harbeck <AHarbeck@student.fullsail.edu>
2022-07-25 09:40:44 -0700

    updating

---
+++ src/app/models/stattype.ts
@@ -0,0 +1,8 @@
+export enum Stattype {
+    HP = 'hp',
+    PA = 'attack',
+    PD = 'defense',
+    SA = 'special-attack',
+    SD = 'special-defense',
+    SP = 'speed'
+}
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,24 +1,45 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError, retry } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Pokemon } from '../models/pokemon';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
+  pokeUrl: string = 'https://pokeapi.co/api/v2/pokemon';
+  pokeData: any;
+
   constructor(private http: HttpClient) { }
 
-  getPokemon(): Observable<Pokemon[]>{
-    return this.http.get(`${environment.apiUrl}/pokemon`, {
-      withCredentials: true
-    }).pipe(
+  getPokemon(): Observable<Pokemon[]> {
+    return this.http.get(`${environment.apiUrl}/pokemon`).pipe(
       map(
         response => response as Pokemon[]
       )
     );
   }
+
+  createPokemon(apiId: number, level: number) {
+    this.http.post(`${environment.apiUrl}/pokemon`,
+      {
+        'apiId': apiId,
+        'level': level
+      },
+      httpOptions).subscribe();
+  }
+
+  fetchPokemon(url: string) {
+    return this.http.get(url);
+  }
+
+  // let s: Stat = new Stat(-1, stat.stat.name, stat.base_stat, (Math.random() * 32), 0);
+  //   myStats.push(s);
 }
