569d317cd3311889c9a5cd891b2e4830e8923cec
calvinpost <107434565+calvinpost@users.noreply.github.com>
2022-07-27 13:06:01 -0400

    Delete pmfor.directive.spec.ts
    Not used

--- src/app/directives/pmfor.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PmforDirective } from './pmfor.directive';
-
-describe('PmforDirective', () => {
-  it('should create an instance', () => {
-    const directive = new PmforDirective();
-    expect(directive).toBeTruthy();
-  });
-});
