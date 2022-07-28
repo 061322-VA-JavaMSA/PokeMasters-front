0914e4bf42cc7ce464862f27bc5be9db8a8a2bfc
Calvin Post <calvinhenry826@revature.net>
2022-07-28 12:23:57 -0400

    Working on Storage

--- src/app/models/pokemon.ts
+++ src/app/models/pokemon.ts
@@ -4,33 +4,80 @@ import { Trainer } from "./trainer";
 export class Pokemon {
     public id: number;
     public apiId: number;
+    public name: string;
     public nickname: string;
+    public sprite: {
+        front: string,
+        back: string,
+        front_a: string,
+        back_a: string
+    };
     public nature: Nature;
     public hp: number;
     public level: number;
     public exp: number;
-    public iv: Map<string, number>;
-    public ev: Map<string, number>;
+    public stats: {
+        hp: number,
+        att: number,
+        def: number,
+        satt: number,
+        sdef: number,
+        speed: number,
+    };
+    public base: {
+        hp: number,
+        att: number,
+        def: number,
+        satt: number,
+        sdef: number,
+        speed: number,
+    };
+    public iv: {
+        hp: number,
+        att: number,
+        def: number,
+        satt: number,
+        sdef: number,
+        speed: number,
+    };
+    public ev: {
+        hp: number,
+        att: number,
+        def: number,
+        satt: number,
+        sdef: number,
+        speed: number,
+    };
+    public baseExp: number;
+    public type1: string;
+    public type2: string;
+    public height: number;
+    public weight: number;
     public shiny: boolean;
-    public stat: Map<string, number>;
     public trainer: Trainer;
     public ot: Trainer;
-    public sprite!: Map<string, string>;
-    public data: any;
-    constructor(id: number, apiId: number, nickname: string, nature: Nature, hp: number, level: number, exp: number, iv: Map<string, number>, ev: Map<string, number>, shiny: boolean, trainer: Trainer, ot: Trainer) {
+    constructor(id: number, apiId: number, name: string, nickname: string, sprite: any, nature: Nature, hp: number, level: number, exp: number, stats: any, base: any, iv: any, ev: any, baseExp: number, type1: string, type2: string, height: number, weight: number, shiny: boolean, trainer: Trainer, ot: Trainer) {
         this.id = id;
         this.apiId = apiId;
+        this.name = name;
         this.nickname = nickname;
+        this.sprite = sprite;
         this.nature = nature;
         this.hp = hp;
         this.level = level;
         this.exp = exp;
+        this.stats = stats;
+        this.base = base;
         this.iv = iv;
         this.ev = ev;
+        this.baseExp = baseExp;
+        this.type1 = type1;
+        this.type2 = type2;
+        this.height = height;
+        this.weight = weight;
         this.shiny = shiny;
         this.trainer = trainer;
         this.ot = ot;
-        this.stat = new Map<string, number>;
     }
 
 }
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,11 +1,11 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, throwError } from 'rxjs';
-import { map, catchError, retry } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
-import { ApiPoke } from '../models/apipoke';
-import { Nature } from '../models/nature.enum';
+import { Party } from '../models/party';
 import { Pokemon } from '../models/pokemon';
+import { Box, Storage } from '../models/storage';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -37,140 +37,75 @@ export class PokemonService {
     return this.http.get(`http://pokeapi.co/api/v2/pokemon/${name}`);
   }
 
-  createPokemon(apiId: number, level: number) {
-    this.http.post(`${environment.apiUrl}/pokemon`,
+  createPokemon(apiId: number, level: number): Observable<Pokemon> {
+    return this.http.post(`${environment.apiUrl}/pokemon`,
       {
         'apiId': apiId,
-        'level': level
+        'level': level,
+        'trainer': {
+          'id': 1,
+          'username': 'calvin',
+          'password': '1234',
+          'displayName': 'pokemaster1',
+          'money': 100,
+          'role': 'TRAINER'
+        }
       },
-      httpOptions).subscribe();
+      httpOptions).pipe(
+        map(
+          response => response as Pokemon
+        )
+      )
   }
 
   fetchPokemon(url: string) {
     return this.http.get(url);
   }
 
-  /*
-  setData(poke: Pokemon) {
-    this.http.get(`https://pokeapi.co/api/v2/pokemon/${poke.apiId}`)
-    .subscribe((data: any) => {
-      poke.data = data;
-    });
-    poke.populate();
-  }
-  */
-
-  populate(p: Pokemon): Pokemon {
-    console.log(p.iv.get('hp'));
-    let sprites = p.data['sprites']['versions']['generation-v']['black-white'];
-    /*
-    if (this.gender == Gender.FEMALE) {
-        if (this.shiny) {
-            this.sprite.front = sprites.front_shiny_female || sprites.front_shiny;
-            this.sprite.back = sprites.back_shiny_female || sprites.back_shiny;
-            this.sprite.front_a = sprites.animated.front_shiny_female || sprites.animated.front_shiny;
-            this.sprite.back_a = sprites.animated.back_shiny_female || sprites.animated.back_shiny;
-        } else {
-            this.sprite.front = sprites.front_female || sprites.front_default;
-            this.sprite.back = sprites.back_female || sprites.back_default;
-            this.sprite.front_a = sprites.animated.front_female || sprites.animated.front_default;
-            this.sprite.back_a = sprites.animated.back_female || sprites.animated.back_default;
-        }
-    } else {
-        if (this.shiny) {
-            this.sprite.front = sprites.front_shiny;
-            this.sprite.back = sprites.back_shiny;
-            this.sprite.front_a = sprites.animated.front_shiny;
-            this.sprite.back_a = sprites.animated.back_shiny;
-        } else {
-            this.sprite.front = sprites.front_default;
-            this.sprite.back = sprites.back_default;
-            this.sprite.front_a = sprites.animated.front_default;
-            this.sprite.back_a = sprites.animated.back_default;
-        }
-    }
-    */
-
-    let map: Map<string, string> = new Map();
-    if (p.shiny) {
-      map.set('front', sprites.front_shiny);
-      map.set('back', sprites.back_shiny);
-      map.set('front_a', sprites.animated.front_shiny);
-      map.set('back_a', sprites.animated.back_shiny);
-    } else {
-      map.set('front', sprites.front_default);
-      map.set('back', sprites.back_default);
-      map.set('front_a', sprites.animated.front_default);
-      map.set('back_a', sprites.animated.back_default);
-    }
-    console.log(map);
-    p.sprite = map;
-
-    let base: any[] = p.data.stats;
-    base.forEach(stat => {
-      let name: string = '';
-
-      switch (stat.stat.name) {
-        case 'hp': name = 'hp'; break;
-        case 'attack': name = 'att'; break;
-        case 'defense': name = 'def'; break;
-        case 'special-attack': name = 'satt'; break;
-        case 'special-defense': name = 'sdef'; break;
-        case 'speed': name = 'speed'; break;
-        default:
-      }
-      let iv: number = p.iv.get(name) || 0;
-      let ev: number = p.ev.get(name) || 0;
-      p.stat.set(name, this.calculateStat(p, name, stat.base_stat, iv, ev));
-    })
-
-    return p;
+  getStorage(): Observable<Storage> {
+    return this.http.get(`${environment.apiUrl}/storage/trainer/calvin`).pipe(
+      map(
+        response => response as Storage
+      )
+    );
   }
 
-  calculateStat(p: Pokemon, name: string, b: number, i: number, e: number) {
-    let stat: number = ((2 * b + i + Math.floor(e / 4)) * p.level) / 100;
-    if (name === 'hp') {
-      return Math.floor(stat + p.level + 10);
-    } else {
-      let n = this.getNatureMod(p, name);
-      return Math.floor((stat + 5) * n);
-    }
+  saveStorage(s: Storage): Observable<Storage> {
+    return this.http.put(`${environment.apiUrl}/storage`,
+    s,
+    httpOptions).pipe(
+      map(
+        response => response as Storage
+      )
+    )
   }
 
-  getNatureMod(p: Pokemon, s: string) {
-    switch (s) {
-      case 'att':
-        switch (p.nature) {
-          case Nature.LONELY, Nature.BRAVE, Nature.ADAMANT, Nature.NAUGHTY: return 1.1;
-          case Nature.BOLD, Nature.TIMID, Nature.MODEST, Nature.CALM: return 0.9;
-          default: return 1.0;
-        }
-      case 'def':
-        switch (p.nature) {
-          case Nature.BOLD, Nature.RELAXED, Nature.IMPISH, Nature.LAX: return 1.1;
-          case Nature.LONELY, Nature.HASTY, Nature.MILD, Nature.GENTLE: return 0.9;
-          default: return 1.0;
-        }
-      case 'satt':
-        switch (p.nature) {
-          case Nature.MODEST, Nature.MILD, Nature.QUIET, Nature.RASH: return 1.1;
-          case Nature.ADAMANT, Nature.IMPISH, Nature.JOLLY, Nature.CAREFUL: return 0.9;
-          default: return 1.0;
-        }
-      case 'sdef':
-        switch (p.nature) {
-          case Nature.CALM, Nature.GENTLE, Nature.SASSY, Nature.CAREFUL: return 1.1;
-          case Nature.NAUGHTY, Nature.LAX, Nature.NAIVE, Nature.RASH: return 0.9;
-          default: return 1.0;
-        }
-      case 'speed':
-        switch (p.nature) {
-          case Nature.TIMID, Nature.HASTY, Nature.JOLLY, Nature.NAIVE: return 1.1;
-          case Nature.BRAVE, Nature.RELAXED, Nature.QUIET, Nature.SASSY: return 0.9;
-          default: return 1.0;
-        }
-      default: return 1.0;
-    }
+  getParty(): Observable<Party> {
+    return this.http.get(`${environment.apiUrl}/party/trainer/calvin`).pipe(
+      map(
+        response => response as Party
+      )
+    );
+  }
+
+  saveBox(b: Box): Observable<Box> {
+    return this.http.put(`${environment.apiUrl}/box`,
+    b,
+    httpOptions).pipe(
+      map(
+        response => response as Box
+      )
+    )
+  }
+
+  saveParty(p: Party): Observable<Party> {
+    return this.http.put(`${environment.apiUrl}/party`,
+    p,
+    httpOptions).pipe(
+      map(
+        response => response as Party
+      )
+    )
   }
 
 }
