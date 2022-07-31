2655a21629ecb4b5a2b45ca007bf0c8648525a98
Calvin Post <calvinhenry826@revature.net>
2022-07-31 15:10:38 -0400

    Working on Trade feature

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,11 +1,9 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Party } from '../models/party';
-import { Pokemon } from '../models/pokemon';
 import { Box, Storage } from '../models/storage';
+import { TokenStorageService } from './token-storage.service';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -19,93 +17,54 @@ export class PokemonService {
   pokeUrl: string = 'https://pokeapi.co/api/v2/pokemon';
   pokeData: any;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private token: TokenStorageService) {}
 
-  getPokemon(id: number): Observable<Pokemon> {
-    return this.http.get(`${environment.apiUrl}/pokemon/${id}`).pipe(
-      map(
-        response => response as Pokemon
-      )
-    );
+  getPokemon(id: number) {
+    return this.http.get(`${environment.apiUrl}/pokemon/${id}`);
   }
 
   getPokemons() {
-    return this.http.get(`http://pokeapi.co/api/v2/pokemon?limit=10`);
+    return this.http.get(`http://pokeapi.co/api/v2/pokemon`);
   }
 
   getMoreData(name: string) {
     return this.http.get(`http://pokeapi.co/api/v2/pokemon/${name}`);
   }
 
-  createPokemon(apiId: number, level: number): Observable<Pokemon> {
-    return this.http.post(`${environment.apiUrl}/pokemon`,
+  createPokemon(apiId: number, level: number){
+    let id = this.token.getDecodedAccessToken()?.id;
+    return this.http.post(`${environment.apiUrl}/pokemon/trainer/${id}`,
       {
         'apiId': apiId,
-        'level': level,
-        'trainer': {
-          'id': 1,
-          'username': 'calvin',
-          'password': '1234',
-          'displayName': 'pokemaster1',
-          'money': 100,
-          'role': 'TRAINER'
-        }
+        'level': level
       },
-      httpOptions).pipe(
-        map(
-          response => response as Pokemon
-        )
-      )
+      httpOptions);
   }
 
   fetchPokemon(url: string) {
     return this.http.get(url);
   }
 
-  getStorage(): Observable<Storage> {
-    return this.http.get(`${environment.apiUrl}/storage/trainer/calvin`).pipe(
-      map(
-        response => response as Storage
-      )
-    );
+  getStorage() {
+    let id = this.token.getDecodedAccessToken()?.id;
+    return this.http.get(`${environment.apiUrl}/storage/trainer/${id}`);
   }
 
-  saveStorage(s: Storage): Observable<Storage> {
-    return this.http.put(`${environment.apiUrl}/storage`,
-    s,
-    httpOptions).pipe(
-      map(
-        response => response as Storage
-      )
-    )
+  saveStorage(s: Storage) {
+    return this.http.put(`${environment.apiUrl}/storage`, s, httpOptions);
   }
 
-  getParty(): Observable<Party> {
-    return this.http.get(`${environment.apiUrl}/party/trainer/calvin`).pipe(
-      map(
-        response => response as Party
-      )
-    );
+  getParty() {
+    let id = this.token.getDecodedAccessToken()?.id;
+    return this.http.get(`${environment.apiUrl}/party/trainer/${id}`);
   }
 
-  saveBox(b: Box): Observable<Box> {
-    return this.http.put(`${environment.apiUrl}/box`,
-    b,
-    httpOptions).pipe(
-      map(
-        response => response as Box
-      )
-    )
+  saveBox(b: Box) {
+    return this.http.put(`${environment.apiUrl}/box`, b, httpOptions);
   }
 
-  saveParty(p: Party): Observable<Party> {
-    return this.http.put(`${environment.apiUrl}/party`,
-    p,
-    httpOptions).pipe(
-      map(
-        response => response as Party
-      )
-    )
+  saveParty(p: Party) {
+    return this.http.put(`${environment.apiUrl}/party`, p, httpOptions);
   }
 
 }
